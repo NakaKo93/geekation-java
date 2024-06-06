f3264c917e45572e174dafb4358e0a15b5bbf590
NakaKo93 <pcdradatatinnsave21@gmail.com>
2024-06-06 18:09:52 +0900

    変数

---
+++ src/basicSyntax/Variables.java
@@ -0,0 +1,18 @@
+package basicSyntax;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int num1;
+		double num2;
+		
+		num1 = 93;
+		num2 = 273.15;
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println("私の好きな整数は" + num1 + "です。");
+		System.out.println("私の好きな少数は" + num2 + "です。");
+	}
+
+}
